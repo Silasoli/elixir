0a7ca62796d7b51299ba124f4154703c881b763f
silasoli <silasoliv39@gmail.com>
2021-05-03 14:09:25 -0300

    add mudança de cor para os botões

--- src/pages/Agendamento/Dose1/AgenDose1.vue
+++ src/pages/Agendamento/Dose1/AgenDose1.vue
@@ -32,7 +32,7 @@
             <button class="days">Sexta</button>
             <button class="days">Sabado</button> 
           </div>
-          <button value="1" v-for="item in 30" :key="item" :class="'selectComponent'+item" id="selectComponent">{{item}}</button> 
+          <button  v-for="item in 30" :key="item" :id="'selectComponentD'+item" class="selectComponent" @click="selectData(`${item}`)">{{item}}</button> 
         </div> 
             <b-button id="btnSave1" class="saveBtn"    @click="salvar" variant="primary">Salvar</b-button>
       </div>
@@ -52,9 +52,11 @@
           <div id="divDay">
            
           </div>
-          <button value="1" v-for="item in 30" :key="item" :class="'selectComponent'+item" id="selectComponent">{{item}}</button> 
+          <div id="hours">
+            <button v-for="item in 30" :key="item" :id="'selectComponentH'+item" class="selectComponent" @click="selectHours(`${item}`)">{{item}}</button> 
+          </div>
         </div> 
-            <b-button id="btnSave1" class="saveBtn"    @click="salvar" variant="primary">Salvar</b-button>
+            <b-button id="btnSave1" class="saveBtn"   variant="primary">Salvar</b-button>
       </div>
   </div>
 </template>
@@ -88,12 +90,37 @@
     salvar: function(){
         document.getElementById('mainDivHorario').style.display = "block";
         document.getElementById('mainDiv').style.display = "none";
-        var dNow = new Date();
-        var hora =  dNow.getHours() + ':'
-        var minutos =dNow.setMinutes(dNow.getMinutes());
+        // var dNow = new Date();
+        // var hora =  dNow.getHours() + ':'/
+        // var minutos =dNow.setMinutes(dNow.getMinutes());
         // minutos = minutos+(dNow.setMinutes(0));
-        var localdate = hora + minutos;
-        console.log(localdate)
+        // var localdate = hora + minutos;
+        // console.log(localdate)
+    },
+    selectData:function(id) {
+       let btn = document.getElementById('selectComponentD'+id)
+      if (btn.classList[0]=="selectComponent") {
+            btn.classList.remove('selectComponent');
+            btn.classList.add('selectComponentChecked');
+      } else if(btn.classList[0]!="selectComponent"){
+            btn.classList.remove('selectComponentChecked');
+            btn.classList.add('selectComponent');
+      }
+    },
+    selectHours: function(id){
+      let btn = document.getElementById('selectComponentH'+id)
+      if (btn.classList[0]=="selectComponent") {
+            document.getElementById('selectComponentH'+id).classList.remove('selectComponent');
+            document.getElementById('selectComponentH'+id).classList.add('selectComponentChecked');
+      } else if(btn.classList[0]!="selectComponent"){
+            document.getElementById('selectComponentH'+id).classList.remove('selectComponentChecked');
+            document.getElementById('selectComponentH'+id).classList.add('selectComponent');
+      }
+    },
+    getbtn: function(a) {
+      console.log(a.id)
+      // console.log(this.id)
+      
     }
   }
  }
@@ -153,7 +180,7 @@
   padding: 16px;
   padding-left: 18px;
 }
-#selectComponent{
+.selectComponent{
   width: 110px;
   height: 50px;
   background-color: #CCBD5A;
@@ -163,9 +190,15 @@
   margin-left: 8px;
   border: 0px;
 }
-#selectComponent::after{
-  background-color: black;
-  
+.selectComponentChecked{
+  width: 110px;
+  height: 50px;
+  background-color: #F45267;
+  margin: 5px;
+  position: relative;
+  color: black;
+  margin-left: 8px;
+  border: 0px;
 }
 .days{
   width: 110px;
