97113ddafc2bfb3d7e0f7589cd8ccbc1de4d6e6d
silasoli <silasoliv39@gmail.com>
2021-05-04 17:13:13 -0300

    remove table agenDose2

--- src/pages/Agendamento/Dose2/AgenDose2.vue
+++ src/pages/Agendamento/Dose2/AgenDose2.vue
@@ -10,37 +10,7 @@
       <b-button id="searchBtn"></b-button>
     </div>
     <div id="table">
-     <div>
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      item-key="name"
-      class="elevation-1"
-      :search="search"
-      :custom-filter="filterOnlyCapsText"
-    >
-      <template v-slot:top>
-        <v-text-field
-          v-model="search"
-          label="Search (UPPER CASE ONLY)"
-          class="mx-4"
-        ></v-text-field>
-      </template>
-      <template v-slot:body.append>
-        <tr>
-          <td></td>
-          <td>
-            <v-text-field
-              v-model="calories"
-              type="number"
-              label="Less than"
-            ></v-text-field>
-          </td>
-          <td colspan="4"></td>
-        </tr>
-      </template>
-    </v-data-table>
-  </div>
+ 
     </div>
   </div>
 </template>
