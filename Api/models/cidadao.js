ee2e516dca3d794bda76432b90a58edb2a07853e
silasoli <silasoliv39@gmail.com>
2021-05-05 10:57:46 -0300

    add api no projeto

---
+++ Api/models/cidadao.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var CidadaoSchema = new Schema({
+    nomeCompleto:String,
+    // dataNascimento:String,
+    // CPF:String,
+    // tel:String,
+    email:String,
+    senha:String
+});
+
+module.exports = mongoose.model('Cidadao' , CidadaoSchema);
